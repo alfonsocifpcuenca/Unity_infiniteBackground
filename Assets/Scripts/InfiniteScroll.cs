c3c83d911f9173c2b935d618ad7174f74f63f48f
alfonsocifpcuenca <alfonso.rodriguez@cifpcuenca.es>
2024-01-19 00:30:06 +0100

    Reordering scripts Infinity script modified

--- Assets/Scripts/InfiniteScroll.cs
+++ Assets/Scripts/InfiniteScroll.cs
@@ -12,22 +12,21 @@ public class InfiniteScroll : MonoBehaviour
     [SerializeField]
     private Camera camera;
 
-    private float cameraWidth;
+    private float spriteWidth;
 
     private void Awake()
     {
         if (this.camera != null)
         {
             this.currentPosition = this.backgroundPosition;
-
-            this.cameraWidth = camera.orthographicSize * 2f * camera.aspect;
+            this.spriteWidth = this.GetComponent<SpriteRenderer>().bounds.size.x;
             SetBackgroundPosition();
         }
     }
 
     private void SetBackgroundPosition()
     {
-        var newPositionX = this.cameraWidth * this.currentPosition;
+        var newPositionX = this.spriteWidth * this.currentPosition;
         var newPositionY = this.transform.localPosition.y;
         var newPositionZ = this.transform.localPosition.z;
         var newPosition = new Vector3(newPositionX, newPositionY, newPositionZ);
@@ -36,19 +35,18 @@ public class InfiniteScroll : MonoBehaviour
 
     void Update()
     {
-        
         float distance = this.camera.gameObject.transform.position.x - this.transform.position.x;
-        if (distance > (this.cameraWidth * 1.5))
+        if (distance > (this.spriteWidth * 1.5))
         {
-            this.currentPosition += 3; 
+            this.currentPosition += 3;
             SetBackgroundPosition();
         }
 
-        if (distance < -(this.cameraWidth * 1.5))
+        if (distance < -(this.spriteWidth * 1.5))
         {
             this.currentPosition -= 3;
             SetBackgroundPosition();
         }
-        
+
     }
 }
